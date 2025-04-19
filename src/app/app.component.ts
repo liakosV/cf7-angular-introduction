028dc9b0aaeec3333e3464be0f677ef551870b0c
LiakosV <billiak@hotmail.com>
2025-04-19 14:44:42 +0300

    feat: import personTableComponent and object person added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = "Vasilis";
+
+  person = {
+    givenName: "Vasilis",
+    surName: "Liakos",
+    age: 25,
+    email: "billiak@aueb.gr"
+  }
 }
